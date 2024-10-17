e3866b91322a40178aca33f38a000f5e54202410
Kurt1-cyber <delutakurtlouie@gmail.com>
2024-10-17 18:40:51 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+const favSingers = ["Snoob Dog", "Post Malone", "Eminem"];
+
+console.log("First Singer:", favSingers[1]);
+
+const FavNumbers = ["string", ["otherarray"], 123, true];
+
+console.log("Item 0:", mixedArr[0]);
+console.log("Item 1:", mixedArr[1]);
+console.log("Item 2:", mixedArr[2]);
+console.log("Item 3:", mixedArr[3]);
